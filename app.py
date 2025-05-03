494650d1a291e213539f59c983a99acbf1864baa
Musaib Bin Bashir <musaibbashir02@gmail.com>
2025-05-03 17:21:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import psycopg2
 from urllib.parse import urlparse
 
 app = Flask(__name__)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 # Database connection handling
 def get_db_connection():
     """
